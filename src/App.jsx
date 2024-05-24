1720d1c2d3e95e90ecaf9ece825b81df8daceedb
Gaurav Kulkarni <gykulkarni23@gmail.com>
2024-05-24 15:28:04 +0530

    appwrite env setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+    
+      <h1>Blog app with AppWrite </h1>
+    </>
+  )
+}
+
+export default App
